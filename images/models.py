95d0fd680cf36dc0f889cdb80ca2f41b5725cb9b
brahim024 <ifninos168@gmail.com>
2020-10-20 18:55:27 +0200

    fix models

--- images/models.py
+++ images/models.py
@@ -3,12 +3,14 @@ from django.conf import settings
 from django.utils.text import slugify
 # Create your models here.
 class Image(models.Model):
-	user=models.ForeignKey(settings.AUTH_MODEL,related_name='created',on_delete=models.CASCADE)
+	user=models.ForeignKey(settings.AUTH_USER_MODEL,related_name='created',on_delete=models.CASCADE)
 	title=models.CharField(max_length=150)
 	slug=models.SlugField(max_length=100,blank=True)
 	url=models.URLField()
 	description=models.TextField(blank=True)
 	created=models.DateTimeField(auto_now=True,db_index=True)
+	user_like=models.ManyToManyField(settings.AUTH_USER_MODEL,
+										related_name='image_likes',blank=True)
 	def __str__(self):
 		return self.title
 
