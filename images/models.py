4f8454017e285190fbeb05478323705605b6ff22
brahim024 <ifninos168@gmail.com>
2020-10-20 18:47:06 +0200

    add image models

--- images/models.py
+++ images/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from django.conf import settings
+from django.utils.text import slugify
 # Create your models here.
+class Image(models.Model):
+	user=models.ForeignKey(settings.AUTH_MODEL,related_name='created',on_delete=models.CASCADE)
+	title=models.CharField(max_length=150)
+	slug=models.SlugField(max_length=100,blank=True)
+	url=models.URLField()
+	description=models.TextField(blank=True)
+	created=models.DateTimeField(auto_now=True,db_index=True)
+	def __str__(self):
+		return self.title
+
+
+	def save(self,*args,**kwargs):
+		if not self.slug:
+			self.slug=slugify(self.title)
+		super().save(*args,**kwargs)
